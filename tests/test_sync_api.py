a7c3b7c1128b82581143fd8f403b20aa643d94cd
ultrasev <i@ultrasev.com>
2024-10-12 17:48:05 +0800

    refactor: rename test file for clarity

--- tests/test_sync_api.py
+++ tests/test_sync_api.py
@@ -3,26 +3,30 @@ import pytest
 import os
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
-import aiohttp
 import random
+from loguru import logger
 load_dotenv()
 
 
 def api_endpoint():
     env = os.environ.get('ENV', 'development')
     if env == 'production':
-        return ""
+        return "https://llmapi-69qqyvcj2-slippertopia.vercel.app/proxy/api.groq.com/openai/v1"
     elif env == 'development':
-        return "http://192.168.31.46:3000/groq"
+        return "http://192.168.31.46:3000"
     else:
         raise ValueError(f"Invalid environment: {env}")
 
+
 BASE_URL = api_endpoint()
+logger.info(f"BASE_URL: {BASE_URL}")
+
+
 async def make_request(api_key: str,
                        model: str,
                        supplier: str,
                        query: str = "what is the result of 2*21"):
-    client = AsyncOpenAI(base_url=BASE_URL, api_key=api_key)
+    client = AsyncOpenAI(base_url=BASE_URL + f"/{supplier}", api_key=api_key)
     query += random.randint(1, 1000) * ' '  # avoid cached result
     response = await client.chat.completions.create(
         model=model,
@@ -40,9 +44,18 @@ async def make_request(api_key: str,
 
 @pytest.mark.asyncio
 async def test_groq():
-    response = await make_request(
+    await make_request(
+        supplier="groq",
         api_key=os.environ["GROQ_API_KEY"],
-        model="llama3-70b-8192",
-        supplier="groq"
+        model="llama3-70b-8192"
     )
-    assert '42' in response
+
+
+@pytest.mark.asyncio
+async def test_openai():
+    await make_request(
+        supplier="openai",
+        api_key=os.environ["OPENAI_API_KEY"],
+        model="gpt-4o-mini"
+    )
+#
