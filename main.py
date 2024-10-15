0331dab4a0a03f4ea2b1387c0808b0d6d83bec52
ultrasev <i@ultrasev.com>
2024-10-15 14:42:25 +0800

    feat: add OpenAI router to FastAPI app

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from api.random import router as random_router
 from api.servers.groq import router as groq_router
 from fastapi import FastAPI
 from fastapi.responses import Response
+from api.servers.openai import router as openai_router
+
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/hello")
 app.include_router(random_router, prefix="/random")
 app.include_router(groq_router, prefix="/groq")
+app.include_router(openai_router, prefix="/openai")
 
 
 @app.get("/")
