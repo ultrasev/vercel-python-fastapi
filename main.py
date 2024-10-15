85b72e37a2ceaf9f2f4c5a52a44e185bf0669c86
ultrasev <i@ultrasev.com>
2024-10-15 16:25:34 +0800

    feat: add gemini router integration

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 from public.usage import USAGE as html
 from api.hello import router as hello_router
-from api.random import router as random_router
 
-
-from api.servers.groq import router as groq_router
 from fastapi import FastAPI
 from fastapi.responses import Response
+from api.servers.groq import router as groq_router
 from api.servers.openai import router as openai_router
+from api.servers.gemini import router as gemini_router
 
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/hello")
-app.include_router(random_router, prefix="/random")
 app.include_router(groq_router, prefix="/groq")
 app.include_router(openai_router, prefix="/openai")
+app.include_router(gemini_router, prefix="/gemini")
 
 
 @app.get("/")
